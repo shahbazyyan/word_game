9812003a7ca77f84f74af04767a60f7956c9bdec
shahbazyyan <shahbazyan.daviit@gmail.com>
2023-11-20 22:17:28 +0400

    Created 3 languages for game, gived new styles, added rules and fixed some bugs

---
+++ js/russian.js
@@ -0,0 +1,61 @@
+function createGame (selectedWords) {
+ 
+    const word = document.querySelector(".word");
+    const timer = document.querySelector(".time > span");
+    const checkWord = document.querySelector(".check");
+    const changeWord = document.querySelector(".refresh");
+    const input = document.querySelector("input");
+    let correctWord, time;
+    
+    function startTimer (maxTime) {
+      clearInterval(time);
+      time = setInterval(() => {
+         if (maxTime > 0) {
+          maxTime --;
+          return timer.textContent = maxTime;
+         };
+         clearInterval(time);
+         alert(`Ваше время ушло. Правильное слово было '${correctWord}'`)
+         initGame();
+      }, 1000);
+    };
+    
+    const initGame = () => {
+      startTimer(30);
+       let randomObj = selectedWords[Math.floor(Math.random() * selectedWords.length)];
+       let wordArray = randomObj.word.split("");
+       for (let i = wordArray.length - 1; i >  0; i--) {
+        let j = Math.floor(Math.random()* (i+1));
+        [wordArray[i], wordArray[j]] = [wordArray[j], wordArray[i]]
+       }
+       word.innerText = wordArray.join("");
+       correctWord = randomObj.word.toLocaleLowerCase();
+       input.value = "";
+       input.setAttribute("maxlength", correctWord.length);
+    };
+    
+    initGame();
+    
+    
+    function checkWords () {
+      let playerWord = input.value.toLocaleLowerCase();
+      if (!playerWord) {
+        alert("Пожалуйста, заполните слово.");
+        return; 
+      };
+    
+      if (playerWord !== correctWord) {
+        alert(` '${playerWord}' было неверно. Попробуйте еще раз!`);
+        return; 
+      };
+    
+      alert('Ты выиграл!');
+      initGame();
+    };
+    
+    changeWord.addEventListener("click", initGame);
+    checkWord.addEventListener("click", checkWords);
+};
+    
+  
+createGame(russianWords);
